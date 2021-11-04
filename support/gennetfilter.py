981e734cd7e5ad853ca96e06c1a3d4dc6edbd0c3
Markus Linnala <markus.linnala@gmail.com>
2021-11-04 12:30:58 +0200

    support/*.py: replace TAB with 4 spaces

--- support/gennetfilter.py
+++ support/gennetfilter.py
@@ -20,144 +20,144 @@ PACKET_INPUT = "_server_packet_t"
 PACKET_OUTPUT = "_client_packet_t"
 
 class Port:
-	def __init__(self, proto, num, mls_sens, mcs_cats=""):
-		# protocol of the port
-		self.proto = proto
+    def __init__(self, proto, num, mls_sens, mcs_cats=""):
+        # protocol of the port
+        self.proto = proto
 
-		# port number
-		self.num = num
+        # port number
+        self.num = num
 
-		# MLS sensitivity
-		self.mls_sens = mls_sens
+        # MLS sensitivity
+        self.mls_sens = mls_sens
 
-		# MCS categories
-		# not currently supported, so we always get s0
-		self.mcs_cats = DEFAULT_MCS
+        # MCS categories
+        # not currently supported, so we always get s0
+        self.mcs_cats = DEFAULT_MCS
 
 class Packet:
-	def __init__(self, prefix, ports):
-		# prefix
-		self.prefix = prefix
+    def __init__(self, prefix, ports):
+        # prefix
+        self.prefix = prefix
 
-		# A list of Ports
-		self.ports = ports
+        # A list of Ports
+        self.ports = ports
 
 def print_input_rules(packets,mls,mcs):
-	line = "base -A selinux_new_input -j SECMARK --selctx system_u:object_r:"+DEFAULT_INPUT_PACKET
-	if mls:
-		line += ":"+DEFAULT_MLS
-	elif mcs:
-		line += ":"+DEFAULT_MCS
+    line = "base -A selinux_new_input -j SECMARK --selctx system_u:object_r:"+DEFAULT_INPUT_PACKET
+    if mls:
+        line += ":"+DEFAULT_MLS
+    elif mcs:
+        line += ":"+DEFAULT_MCS
 
-	print line
+    print line
 
-	for i in packets:
-		for j in i.ports:
-			line="base -A selinux_new_input -p "+j.proto+" --dport "+j.num+" -j SECMARK --selctx system_u:object_r:"+i.prefix+PACKET_INPUT
-			if mls:
-				line += ":"+j.mls_sens
-			elif mcs:
-				line += ":"+j.mcs_cats
-			print line
+    for i in packets:
+        for j in i.ports:
+            line="base -A selinux_new_input -p "+j.proto+" --dport "+j.num+" -j SECMARK --selctx system_u:object_r:"+i.prefix+PACKET_INPUT
+            if mls:
+                line += ":"+j.mls_sens
+            elif mcs:
+                line += ":"+j.mcs_cats
+            print line
 
-	print "post -A selinux_new_input -j CONNSECMARK --save"
-	print "post -A selinux_new_input -j RETURN"
+    print "post -A selinux_new_input -j CONNSECMARK --save"
+    print "post -A selinux_new_input -j RETURN"
 
 def print_output_rules(packets,mls,mcs):
-	line = "base -A selinux_new_output -j SECMARK --selctx system_u:object_r:"+DEFAULT_OUTPUT_PACKET
-	if mls:
-		line += ":"+DEFAULT_MLS
-	elif mcs:
-		line += ":"+DEFAULT_MCS
-	print line
+    line = "base -A selinux_new_output -j SECMARK --selctx system_u:object_r:"+DEFAULT_OUTPUT_PACKET
+    if mls:
+        line += ":"+DEFAULT_MLS
+    elif mcs:
+        line += ":"+DEFAULT_MCS
+    print line
 
-	for i in packets:
-		for j in i.ports:
-			line = "base -A selinux_new_output -p "+j.proto+" --dport "+j.num+" -j SECMARK --selctx system_u:object_r:"+i.prefix+PACKET_OUTPUT
-			if mls:
-				line += ":"+j.mls_sens
-			elif mcs:
-				line += ":"+j.mcs_cats
-			print line
+    for i in packets:
+        for j in i.ports:
+            line = "base -A selinux_new_output -p "+j.proto+" --dport "+j.num+" -j SECMARK --selctx system_u:object_r:"+i.prefix+PACKET_OUTPUT
+            if mls:
+                line += ":"+j.mls_sens
+            elif mcs:
+                line += ":"+j.mcs_cats
+            print line
 
-	print "post -A selinux_new_output -j CONNSECMARK --save"
-	print "post -A selinux_new_output -j RETURN"
+    print "post -A selinux_new_output -j CONNSECMARK --save"
+    print "post -A selinux_new_output -j RETURN"
 
 def parse_corenet(file_name):
-	packets = []
+    packets = []
 
-	corenet_te_in = open(file_name, "r")
+    corenet_te_in = open(file_name, "r")
 
-	while True:
-		corenet_line = corenet_te_in.readline()
+    while True:
+        corenet_line = corenet_te_in.readline()
 
-		# If EOF has been reached:
-		if not corenet_line:
-			break
+        # If EOF has been reached:
+        if not corenet_line:
+            break
 
-		if NETPORT.match(corenet_line):
-			corenet_line = corenet_line.strip();
+        if NETPORT.match(corenet_line):
+            corenet_line = corenet_line.strip();
 
-			# parse out the parameters
-			openparen = string.find(corenet_line,'(')+1
-			closeparen = string.find(corenet_line,')',openparen)
-			parms = re.split('\W+',corenet_line[openparen:closeparen])
-			name = parms[0]
-			del parms[0];
+            # parse out the parameters
+            openparen = string.find(corenet_line,'(')+1
+            closeparen = string.find(corenet_line,')',openparen)
+            parms = re.split('\W+',corenet_line[openparen:closeparen])
+            name = parms[0]
+            del parms[0];
 
-			ports = []
-			while len(parms) > 0:
-				# add a port combination.
-				ports.append(Port(parms[0],parms[1],parms[2]))
-				del parms[:3]
+            ports = []
+            while len(parms) > 0:
+                # add a port combination.
+                ports.append(Port(parms[0],parms[1],parms[2]))
+                del parms[:3]
 
-			packets.append(Packet(name,ports))
-		
-	corenet_te_in.close()
+            packets.append(Packet(name,ports))
+        
+    corenet_te_in.close()
 
-	return packets
+    return packets
 
 def print_netfilter_config(packets,mls,mcs):
-	print "pre *mangle"
-	print "pre :PREROUTING ACCEPT [0:0]"
-	print "pre :INPUT ACCEPT [0:0]"
-	print "pre :FORWARD ACCEPT [0:0]"
-	print "pre :OUTPUT ACCEPT [0:0]"
-	print "pre :POSTROUTING ACCEPT [0:0]"
-	print "pre :selinux_input - [0:0]"
-	print "pre :selinux_output - [0:0]"
-	print "pre :selinux_new_input - [0:0]"
-	print "pre :selinux_new_output - [0:0]"
-	print "pre -A INPUT -j selinux_input"
-	print "pre -A OUTPUT -j selinux_output"
-	print "pre -A selinux_input -m state --state NEW -j selinux_new_input"
-	print "pre -A selinux_input -m state --state RELATED,ESTABLISHED -j CONNSECMARK --restore"
-	print "pre -A selinux_output -m state --state NEW -j selinux_new_output"
-	print "pre -A selinux_output -m state --state RELATED,ESTABLISHED -j CONNSECMARK --restore"
-	print_input_rules(packets,mls,mcs)
-	print_output_rules(packets,mls,mcs)
-	print "post COMMIT"
+    print "pre *mangle"
+    print "pre :PREROUTING ACCEPT [0:0]"
+    print "pre :INPUT ACCEPT [0:0]"
+    print "pre :FORWARD ACCEPT [0:0]"
+    print "pre :OUTPUT ACCEPT [0:0]"
+    print "pre :POSTROUTING ACCEPT [0:0]"
+    print "pre :selinux_input - [0:0]"
+    print "pre :selinux_output - [0:0]"
+    print "pre :selinux_new_input - [0:0]"
+    print "pre :selinux_new_output - [0:0]"
+    print "pre -A INPUT -j selinux_input"
+    print "pre -A OUTPUT -j selinux_output"
+    print "pre -A selinux_input -m state --state NEW -j selinux_new_input"
+    print "pre -A selinux_input -m state --state RELATED,ESTABLISHED -j CONNSECMARK --restore"
+    print "pre -A selinux_output -m state --state NEW -j selinux_new_output"
+    print "pre -A selinux_output -m state --state RELATED,ESTABLISHED -j CONNSECMARK --restore"
+    print_input_rules(packets,mls,mcs)
+    print_output_rules(packets,mls,mcs)
+    print "post COMMIT"
 
 mls = False
 mcs = False
 
 try:
-	opts, paths = getopt.getopt(sys.argv[1:],'mc',['mls','mcs'])
+    opts, paths = getopt.getopt(sys.argv[1:],'mc',['mls','mcs'])
 except getopt.GetoptError, error:
-	print "Invalid options."
-	sys.exit(1)
+    print "Invalid options."
+    sys.exit(1)
 
 for o, a in opts:
-	if o in ("-c","--mcs"):
-		mcs = True
-	if o in ("-m","--mls"):
-		mls = True
+    if o in ("-c","--mcs"):
+        mcs = True
+    if o in ("-m","--mls"):
+        mls = True
 
 if len(paths) == 0:
-	sys.stderr.write("Need a path for corenetwork.te.in!\n")
-	sys.exit(1)
+    sys.stderr.write("Need a path for corenetwork.te.in!\n")
+    sys.exit(1)
 elif len(paths) > 1:
-	sys.stderr.write("Ignoring extra specified paths\n")
+    sys.stderr.write("Ignoring extra specified paths\n")
 
 packets=parse_corenet(paths[0])
 print_netfilter_config(packets,mls,mcs)
--- support/segenxml.py
+++ support/segenxml.py
@@ -11,8 +11,8 @@
 #      the Free Software Foundation, version 2.
 
 """
-	This script generates XML documentation information for layers specified
-	by the user.
+    This script generates XML documentation information for layers specified
+    by the user.
 """
 
 import sys
@@ -68,276 +68,276 @@ XML_COMMENT = re.compile("^##\s+(.*?)\s*$")
 
 # FUNCTIONS
 def getModuleXML(file_name):
-	'''
-	Returns the XML data for a module in a list, one line per list item.
-	'''
+    '''
+    Returns the XML data for a module in a list, one line per list item.
+    '''
 
-	# Gather information.
-	module_dir = os.path.dirname(file_name)
-	module_name = os.path.basename(file_name)
-	module_te = "%s/%s.te" % (module_dir, module_name)
-	module_if = "%s/%s.if" % (module_dir, module_name)
+    # Gather information.
+    module_dir = os.path.dirname(file_name)
+    module_name = os.path.basename(file_name)
+    module_te = "%s/%s.te" % (module_dir, module_name)
+    module_if = "%s/%s.if" % (module_dir, module_name)
 
-	# Try to open the file, if it cant, just ignore it.
-	try:
-		module_file = open(module_if, "r")
-		module_code = module_file.readlines()
-		module_file.close()
-	except:
-		warning("cannot open file %s for read, skipping" % file_name)
-		return []
+    # Try to open the file, if it cant, just ignore it.
+    try:
+        module_file = open(module_if, "r")
+        module_code = module_file.readlines()
+        module_file.close()
+    except:
+        warning("cannot open file %s for read, skipping" % file_name)
+        return []
 
-	module_buf = []
+    module_buf = []
 
-	# Infer the module name, which is the base of the file name.
-	module_buf.append("<module name=\"%s\" filename=\"%s\">\n" 
-		% (os.path.splitext(os.path.split(file_name)[-1])[0], module_if))
+    # Infer the module name, which is the base of the file name.
+    module_buf.append("<module name=\"%s\" filename=\"%s\">\n" 
+        % (os.path.splitext(os.path.split(file_name)[-1])[0], module_if))
 
-	temp_buf = []
-	interface = None
+    temp_buf = []
+    interface = None
 
-	# finding_header is a flag to denote whether we are still looking
-	#  for the XML documentation at the head of the file.
-	finding_header = True
+    # finding_header is a flag to denote whether we are still looking
+    #  for the XML documentation at the head of the file.
+    finding_header = True
 
-	# Get rid of whitespace at top of file
-	while(module_code and module_code[0].isspace()):
-		module_code = module_code[1:]
+    # Get rid of whitespace at top of file
+    while(module_code and module_code[0].isspace()):
+        module_code = module_code[1:]
 
-	# Go line by line and figure out what to do with it.
-	line_num = 0
-	for line in module_code:
-		line_num += 1
-		if finding_header:
-			# If there is a XML comment, add it to the temp buffer.
-			comment = XML_COMMENT.match(line)
-			if comment:
-				temp_buf.append(comment.group(1) + "\n")
-				continue
+    # Go line by line and figure out what to do with it.
+    line_num = 0
+    for line in module_code:
+        line_num += 1
+        if finding_header:
+            # If there is a XML comment, add it to the temp buffer.
+            comment = XML_COMMENT.match(line)
+            if comment:
+                temp_buf.append(comment.group(1) + "\n")
+                continue
 
-			# Once a line that is not an XML comment is reached,
-			#  either put the XML out to module buffer as the
-			#  module's documentation, or attribute it to an
-			#  interface/template.
-			elif temp_buf:
-				finding_header = False
-				interface = INTERFACE.match(line)
-				if not interface:
-					module_buf += temp_buf
-					temp_buf = []
-					continue
+            # Once a line that is not an XML comment is reached,
+            #  either put the XML out to module buffer as the
+            #  module's documentation, or attribute it to an
+            #  interface/template.
+            elif temp_buf:
+                finding_header = False
+                interface = INTERFACE.match(line)
+                if not interface:
+                    module_buf += temp_buf
+                    temp_buf = []
+                    continue
 
-		# Skip over empty lines
-		if line.isspace():
-			continue
+        # Skip over empty lines
+        if line.isspace():
+            continue
 
-		# Grab a comment and add it to the temprorary buffer, if it
-		#  is there.
-		comment = XML_COMMENT.match(line)
-		if comment:
-			temp_buf.append(comment.group(1) + "\n")
-			continue
+        # Grab a comment and add it to the temprorary buffer, if it
+        #  is there.
+        comment = XML_COMMENT.match(line)
+        if comment:
+            temp_buf.append(comment.group(1) + "\n")
+            continue
 
-		# Grab the interface information. This is only not true when
-		#  the interface is at the top of the file and there is no
-		#  documentation for the module.
-		if not interface:
-			interface = INTERFACE.match(line)
-		if interface:
-			# Add the opening tag for the interface/template
-			groups = interface.groups()
-			module_buf.append("<%s name=\"%s\" lineno=\"%s\">\n" % (groups[0], groups[1], line_num))
+        # Grab the interface information. This is only not true when
+        #  the interface is at the top of the file and there is no
+        #  documentation for the module.
+        if not interface:
+            interface = INTERFACE.match(line)
+        if interface:
+            # Add the opening tag for the interface/template
+            groups = interface.groups()
+            module_buf.append("<%s name=\"%s\" lineno=\"%s\">\n" % (groups[0], groups[1], line_num))
 
-			# Add all the comments attributed to this interface to
-			#  the module buffer.
-			if temp_buf:
-				module_buf += temp_buf
-				temp_buf = []
+            # Add all the comments attributed to this interface to
+            #  the module buffer.
+            if temp_buf:
+                module_buf += temp_buf
+                temp_buf = []
 
-			# Add default summaries and parameters so that the
-			#  DTD is happy.
-			else:
-				warning ("%s:%s: unable to find XML for %s %s()" % (file_name, line_num, groups[0], groups[1]))
-				module_buf.append("<summary>\n")
-				module_buf.append("Summary is missing!\n")
-				module_buf.append("</summary>\n")
-				module_buf.append("<param name=\"?\">\n")
-				module_buf.append("<summary>\n")
-				module_buf.append("Parameter descriptions are missing!\n")
-				module_buf.append("</summary>\n")
-				module_buf.append("</param>\n")
+            # Add default summaries and parameters so that the
+            #  DTD is happy.
+            else:
+                warning ("%s:%s: unable to find XML for %s %s()" % (file_name, line_num, groups[0], groups[1]))
+                module_buf.append("<summary>\n")
+                module_buf.append("Summary is missing!\n")
+                module_buf.append("</summary>\n")
+                module_buf.append("<param name=\"?\">\n")
+                module_buf.append("<summary>\n")
+                module_buf.append("Parameter descriptions are missing!\n")
+                module_buf.append("</summary>\n")
+                module_buf.append("</param>\n")
 
-			# Close the interface/template tag.
-			module_buf.append("</%s>\n" % interface.group(1))
+            # Close the interface/template tag.
+            module_buf.append("</%s>\n" % interface.group(1))
 
-			interface = None
-			continue
+            interface = None
+            continue
 
 
 
-	# If the file just had a header, add the comments to the module buffer.
-	if finding_header:
-		module_buf += temp_buf
-	# Otherwise there are some lingering XML comments at the bottom, warn
-	#  the user.
-	elif temp_buf:
-		warning("orphan XML comments at bottom of file %s\n%s" % (file_name, temp_buf))
+    # If the file just had a header, add the comments to the module buffer.
+    if finding_header:
+        module_buf += temp_buf
+    # Otherwise there are some lingering XML comments at the bottom, warn
+    #  the user.
+    elif temp_buf:
+        warning("orphan XML comments at bottom of file %s\n%s" % (file_name, temp_buf))
 
-	# Process the TE file if it exists.
-	module_buf = module_buf + getTunableXML(module_te, "both")
+    # Process the TE file if it exists.
+    module_buf = module_buf + getTunableXML(module_te, "both")
 
-	module_buf.append("</module>\n")
+    module_buf.append("</module>\n")
 
-	return module_buf
+    return module_buf
 
 def getTunableXML(file_name, kind):
-	'''
-	Return all the XML for the tunables/bools in the file specified.
-	'''
+    '''
+    Return all the XML for the tunables/bools in the file specified.
+    '''
 
-	# Try to open the file, if it cant, just ignore it.
-	try:
-		tunable_file = open(file_name, "r")
-		tunable_code = tunable_file.readlines()
-		tunable_file.close()
-	except:
-		warning("cannot open file %s for read, skipping" % file_name)
-		return []
+    # Try to open the file, if it cant, just ignore it.
+    try:
+        tunable_file = open(file_name, "r")
+        tunable_code = tunable_file.readlines()
+        tunable_file.close()
+    except:
+        warning("cannot open file %s for read, skipping" % file_name)
+        return []
 
-	tunable_buf = []
-	temp_buf = []
+    tunable_buf = []
+    temp_buf = []
 
-	# Find tunables and booleans line by line and use the comments above
-	# them.
-	for line in tunable_code:
-		# If it is an XML comment, add it to the buffer and go on.
-		comment = XML_COMMENT.match(line)
-		if comment:
-			temp_buf.append(comment.group(1) + "\n")
-			continue
+    # Find tunables and booleans line by line and use the comments above
+    # them.
+    for line in tunable_code:
+        # If it is an XML comment, add it to the buffer and go on.
+        comment = XML_COMMENT.match(line)
+        if comment:
+            temp_buf.append(comment.group(1) + "\n")
+            continue
 
-		# Get the boolean/tunable data.
-		boolean = BOOLEAN.match(line)
+        # Get the boolean/tunable data.
+        boolean = BOOLEAN.match(line)
 
-		# If we reach a boolean/tunable declaration, attribute all XML
-		#  in the temp buffer to it and add XML to the tunable buffer.
-		if boolean:
-			# If there is a gen_bool in a tunable file or a
-			# gen_tunable in a boolean file, error and exit.
-			# Skip if both kinds are valid.
-			if kind != "both":
-				if boolean.group(1) != kind:
-					error("%s in a %s file." % (boolean.group(1), kind))
+        # If we reach a boolean/tunable declaration, attribute all XML
+        #  in the temp buffer to it and add XML to the tunable buffer.
+        if boolean:
+            # If there is a gen_bool in a tunable file or a
+            # gen_tunable in a boolean file, error and exit.
+            # Skip if both kinds are valid.
+            if kind != "both":
+                if boolean.group(1) != kind:
+                    error("%s in a %s file." % (boolean.group(1), kind))
 
-			tunable_buf.append("<%s name=\"%s\" dftval=\"%s\">\n" % boolean.groups())
-			tunable_buf += temp_buf
-			temp_buf = []
-			tunable_buf.append("</%s>\n" % boolean.group(1))
+            tunable_buf.append("<%s name=\"%s\" dftval=\"%s\">\n" % boolean.groups())
+            tunable_buf += temp_buf
+            temp_buf = []
+            tunable_buf.append("</%s>\n" % boolean.group(1))
 
-	# If there are XML comments at the end of the file, they arn't
-	# attributed to anything. These are ignored.
-	if temp_buf:
-		warning("orphan XML comments at bottom of file %s\n%s" % (file_name, temp_buf))
+    # If there are XML comments at the end of the file, they arn't
+    # attributed to anything. These are ignored.
+    if temp_buf:
+        warning("orphan XML comments at bottom of file %s\n%s" % (file_name, temp_buf))
 
 
-	# If the caller requested a the global_tunables and global_booleans to be
-	# output to a file output them now
-	if len(output_dir) > 0:
-		xmlfile = os.path.split(file_name)[1] + ".xml"
+    # If the caller requested a the global_tunables and global_booleans to be
+    # output to a file output them now
+    if len(output_dir) > 0:
+        xmlfile = os.path.split(file_name)[1] + ".xml"
 
-		try:
-			xml_outfile = open(output_dir + "/" + xmlfile, "w")
-			for tunable_line in tunable_buf:
-				xml_outfile.write (tunable_line)
-			xml_outfile.close()
-		except:
-			warning ("cannot write to file %s, skipping creation" % xmlfile)
+        try:
+            xml_outfile = open(output_dir + "/" + xmlfile, "w")
+            for tunable_line in tunable_buf:
+                xml_outfile.write (tunable_line)
+            xml_outfile.close()
+        except:
+            warning ("cannot write to file %s, skipping creation" % xmlfile)
 
-	return tunable_buf
+    return tunable_buf
 
 def getXMLFileContents (file_name):
-	'''
-	Return all the XML in the file specified.
-	'''
+    '''
+    Return all the XML in the file specified.
+    '''
 
-	tunable_buf = []
-	# Try to open the xml file for this type of file
-	# append the contents to the buffer.
-	try:
-		tunable_xml = open(file_name, "r")
-		tunable_buf += tunable_xml.readlines()
-		tunable_xml.close()
-	except:
-		warning("cannot open file %s for read, assuming no data" % file_name)
+    tunable_buf = []
+    # Try to open the xml file for this type of file
+    # append the contents to the buffer.
+    try:
+        tunable_xml = open(file_name, "r")
+        tunable_buf += tunable_xml.readlines()
+        tunable_xml.close()
+    except:
+        warning("cannot open file %s for read, assuming no data" % file_name)
 
-	return tunable_buf
+    return tunable_buf
 
 def getPolicyXML():
-	'''
-	Return the compelete reference policy XML documentation through a list,
-	one line per item.
-	'''
+    '''
+    Return the compelete reference policy XML documentation through a list,
+    one line per item.
+    '''
 
-	policy_buf = []
-	policy_buf.append("<policy>\n")
+    policy_buf = []
+    policy_buf.append("<policy>\n")
 
-	# Add to the XML each layer specified by the user.
-	for layer in layers.keys ():
-		policy_buf += getLayerXML(layer, layers[layer])
+    # Add to the XML each layer specified by the user.
+    for layer in layers.keys ():
+        policy_buf += getLayerXML(layer, layers[layer])
 
-	# Add to the XML each tunable file specified by the user.
-	for tunable_file in tunable_files:
-		policy_buf += getTunableXML(tunable_file, "tunable")
+    # Add to the XML each tunable file specified by the user.
+    for tunable_file in tunable_files:
+        policy_buf += getTunableXML(tunable_file, "tunable")
 
-	# Add to the XML each XML tunable file specified by the user.
-	for tunable_file in xml_tunable_files:
-		policy_buf += getXMLFileContents (tunable_file)
+    # Add to the XML each XML tunable file specified by the user.
+    for tunable_file in xml_tunable_files:
+        policy_buf += getXMLFileContents (tunable_file)
 
-	# Add to the XML each bool file specified by the user.
-	for bool_file in bool_files:
-		policy_buf += getTunableXML(bool_file, "bool")
+    # Add to the XML each bool file specified by the user.
+    for bool_file in bool_files:
+        policy_buf += getTunableXML(bool_file, "bool")
 
-	# Add to the XML each XML bool file specified by the user.
-	for bool_file in xml_bool_files:
-		policy_buf += getXMLFileContents (bool_file)
+    # Add to the XML each XML bool file specified by the user.
+    for bool_file in xml_bool_files:
+        policy_buf += getXMLFileContents (bool_file)
 
-	policy_buf.append("</policy>\n")
+    policy_buf.append("</policy>\n")
 
-	return policy_buf
+    return policy_buf
 
 def usage():
-	"""
-	Displays a message describing the proper usage of this script.
-	"""
+    """
+    Displays a message describing the proper usage of this script.
+    """
 
-	sys.stdout.write("usage: %s [-w] [-mtb] <file>\n\n" % sys.argv[0])
-	sys.stdout.write("-w --warn\t\t\tshow warnings\n"+\
-	"-m --module <file>\t\tname of module to process\n"+\
-	"-t --tunable <file>\t\tname of global tunable file to process\n"+\
-	"-b --boolean <file>\t\tname of global boolean file to process\n\n")
+    sys.stdout.write("usage: %s [-w] [-mtb] <file>\n\n" % sys.argv[0])
+    sys.stdout.write("-w --warn\t\t\tshow warnings\n"+\
+    "-m --module <file>\t\tname of module to process\n"+\
+    "-t --tunable <file>\t\tname of global tunable file to process\n"+\
+    "-b --boolean <file>\t\tname of global boolean file to process\n\n")
 
-	sys.stdout.write("examples:\n")
-	sys.stdout.write("> %s -w -m policy/modules/apache\n" % sys.argv[0])
-	sys.stdout.write("> %s -t policy/global_tunables\n" % sys.argv[0])
+    sys.stdout.write("examples:\n")
+    sys.stdout.write("> %s -w -m policy/modules/apache\n" % sys.argv[0])
+    sys.stdout.write("> %s -t policy/global_tunables\n" % sys.argv[0])
 
 def warning(description):
-	'''
-	Warns the user of a non-critical error.
-	'''
+    '''
+    Warns the user of a non-critical error.
+    '''
 
-	if warn:
-		sys.stderr.write("%s: " % sys.argv[0] )
-		sys.stderr.write("warning: " + description + "\n")
+    if warn:
+        sys.stderr.write("%s: " % sys.argv[0] )
+        sys.stderr.write("warning: " + description + "\n")
 
 def error(description):
-	'''
-	Describes an error and exists the program.
-	'''
+    '''
+    Describes an error and exists the program.
+    '''
 
-	sys.stderr.write("%s: " % sys.argv[0] )
-	sys.stderr.write("error: " + description + "\n")
-	sys.stderr.flush()
-	sys.exit(1)
+    sys.stderr.write("%s: " % sys.argv[0] )
+    sys.stderr.write("error: " + description + "\n")
+    sys.stderr.flush()
+    sys.exit(1)
 
 
 
@@ -351,41 +351,41 @@ boolean = False
 
 # Check that there are command line arguments.
 if len(sys.argv) <= 1:
-	usage()
-	sys.exit(1)
+    usage()
+    sys.exit(1)
 
 # Parse command line args
 try:
-	opts, args = getopt.getopt(sys.argv[1:], 'whm:t:b:', ['warn', 'help', 'module=', 'tunable=', 'boolean='])
+    opts, args = getopt.getopt(sys.argv[1:], 'whm:t:b:', ['warn', 'help', 'module=', 'tunable=', 'boolean='])
 except getopt.GetoptError:
-	usage()
-	sys.exit(2)
+    usage()
+    sys.exit(2)
 for o, a in opts:
-	if o in ('-w', '--warn'):
-		warn = True
-	elif o in ('-h', '--help'):
-		usage()
-		sys.exit(0)
-	elif o in ('-m', '--module'):
-		module = a
-		break
-	elif o in ('-t', '--tunable'):
-		tunable = a
-		break
-	elif o in ('-b', '--boolean'):
-		boolean = a
-		break
-	else:
-		usage()
-		sys.exit(2)
+    if o in ('-w', '--warn'):
+        warn = True
+    elif o in ('-h', '--help'):
+        usage()
+        sys.exit(0)
+    elif o in ('-m', '--module'):
+        module = a
+        break
+    elif o in ('-t', '--tunable'):
+        tunable = a
+        break
+    elif o in ('-b', '--boolean'):
+        boolean = a
+        break
+    else:
+        usage()
+        sys.exit(2)
 
 if module:
-	sys.stdout.writelines(getModuleXML(module))
+    sys.stdout.writelines(getModuleXML(module))
 elif tunable:
-	sys.stdout.writelines(getTunableXML(tunable, "tunable"))
+    sys.stdout.writelines(getTunableXML(tunable, "tunable"))
 elif boolean:
-	sys.stdout.writelines(getTunableXML(boolean, "bool"))
+    sys.stdout.writelines(getTunableXML(boolean, "bool"))
 else:
-	usage()
-	sys.exit(2)
+    usage()
+    sys.exit(2)
 
