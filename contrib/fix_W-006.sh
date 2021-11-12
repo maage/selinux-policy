fd96048f2c8d0481674b5727608be443cc59a68c
Markus Linnala <markus.linnala@gmail.com>
2021-11-12 21:28:23 +0200

    contrib/fix_W-006.sh

--- contrib/fix_W-006.sh
+++ contrib/fix_W-006.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-set -ex
+set -epux -o pipefail
 
-selint --source --only-enabled --enable=W-006 --recursive policy|grep -Ev '_stub_|_stub |corenet_enable_unlabeled_packets'
+selint --source --only-enabled --enable=W-006 --recursive policy | \
+grep -Ev '_stub_|_stub |corenet_enable_unlabeled_packets'
