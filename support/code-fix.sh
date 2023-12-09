2bf33aa795448cac827f088b7aac85ae5c277a40
Markus Linnala <markus.linnala@gmail.com>
2023-12-10 01:38:45 +0200

    LOCAL: update ./support/code-fix.sh

--- support/code-fix.sh
+++ support/code-fix.sh
@@ -9,7 +9,6 @@ sed -Ei 's/[[:blank:]]+$//' "${py_files[@]}"
 ! git status --porcelain | grep -Ev '^[?][?] ' || exit 1
 fi
 
-if false; then
 sed -Ei '
     s/^(\s*)\t/\1    /g;
     s/^(\s*)\t/\1    /g;
@@ -20,7 +19,6 @@ sed -Ei '
     s/^(\s*)\t/\1    /g
 ' "${py_files[@]}"
 ! git status --porcelain | grep -Ev '^[?][?] ' || exit 1
-fi
 
 if false; then
 ! pyflakes "${py_files[@]}" |& grep 'invalid syntax' || exit 1
@@ -43,6 +41,10 @@ fi
 
 
 ruf=(
+    D206
+    E101
+    SIM109
+    W191
 )
 if (( ${#ruf[@]} )); then
 ruf2="${ruf[*]}"
