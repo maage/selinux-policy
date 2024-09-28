cebf6177b58b0d8895f7d5bcef5da89cf533359c
Markus Linnala <markus.linnala@gmail.com>
2024-09-28 18:34:02 +0300

    support/my-test.sh

--- support/my-test.sh
+++ support/my-test.sh
@@ -4,4 +4,5 @@ set -epux -o pipefail
 
 make bare
 mkdir -p tmp/install/var/lib/selinux/targeted
+cp dist/targeted/modules.conf policy
 make -j"$(nproc)" -O --warn-undefined-variables DISTRO=redhat UBAC=n DIRECT_INITRC=n MONOLITHIC=n MLS_CATS=1024 MCS_CATS=1024 UNK_PERMS=allow NAME=targeted TYPE=mcs DESTDIR=tmp/install 'SEMODULE=/usr/sbin/semodule -v -p tmp/install -X 100 ' base.pp validate modules install install-appconfig load
